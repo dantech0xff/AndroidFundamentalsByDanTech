57316405fec32259c42b7c9d40c94b4252b15b3c
Dan Tech <danhtran.dev@outlook.com>
2024-05-19 09:46:26 +0700

    Add Leak Canary

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,6 +44,9 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+
+    debugImplementation(libs.squareup.leakcanary)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
